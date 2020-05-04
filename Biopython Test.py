729056d977e82a22f79681c0051c1ce5f098188f
albgavm <albgavm@gmail.com>
2020-05-04 11:49:30 -0400

    Makeseq and Transcribe Draft

--- Biopython Test.py
+++ Biopython Test.py
@@ -4,16 +4,29 @@ class dna():
 
     def __init__(self, seq):
         self.seq = seq
+        return [char for char in self.seq]
 
     def makeseq(self):
-        self.list = yield(for char in self.seq)
         # makes a sequence list from a string
+        self.list = yield(for char in self.seq)
+
+    def show(self):
+        #makes a string from self.list
 
     def transcribe(self):
-        for na in self.list, if na == "a" then = "u"
-
+        # converts the a to u
+        for na in self.list:
+            if na == "a":
+                na = "u"
+            elif na == "A":
+                na = "U"
+            else:
+                return
     def translate():
-        #translates directly to protein
+        # translates directly to protein
+        for na in self.list:
+            if na == ""
+
 
     def search():
         #searches the sequence for a pattern
@@ -22,6 +35,6 @@ class dna():
         #searches the pattern in the instance of the class
         #how many patterns are in the DNA sequence
 
-    def
-
+    def reversetranscribe(self):
+        #reverse transcribes from RNA to DNA
         attributes = [a for a in dir(seq) if not a.startswith("_")
