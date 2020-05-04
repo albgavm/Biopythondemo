30ac0278168d998f0695192564a0d714f6a26934
albgavm <albgavm@gmail.com>
2020-05-03 23:33:19 -0400

    Biopython class methods

---
+++ Biopython Test.py
@@ -0,0 +1,25 @@
+
+
+class dna():
+
+    def __init__(self, seq):
+        self.seq = seq
+
+    def makeseq():
+        # makes a sequence list from a string
+
+    def transcribe():
+
+    def translate():
+        #translates directly to protein
+
+    def search():
+        #searches the sequence for a pattern
+
+    def count():
+        #searches the pattern in the instance of the class
+        #how many patterns are in the DNA sequence
+
+    def
+
+        attributes = [a for a in dir(seq) if not a.startswith("_")
